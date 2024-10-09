1cd261184c67331c2b32efb0b65aef5089ae4838
Ragnar20-03 <roshanpp20@gmail.com>
2024-10-09 20:54:53 +0530

    Checkpoint : Navbar

--- frontend/src/components/User/NavBar.tsx
+++ frontend/src/components/User/NavBar.tsx
@@ -103,16 +103,22 @@ export default function Navbar() {
             </div>
           </div>
 
-          <div className="flex md:hidden">
+          <div className="flex md:hidden ">
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset "
             >
               <span className="sr-only">Open main menu</span>
               {isMobileMenuOpen ? (
-                <X className="block h-6 w-6" aria-hidden="true" />
+                <X
+                  className="block h-6 w-6 text-white hover:text-black "
+                  aria-hidden="true"
+                />
               ) : (
-                <Menu className="block h-6 w-6" aria-hidden="true" />
+                <Menu
+                  className="block h-6 w-6 text-white hover:text-black"
+                  aria-hidden="true"
+                />
               )}
             </button>
           </div>
